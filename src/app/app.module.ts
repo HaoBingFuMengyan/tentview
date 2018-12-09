83bd90fa7fe00891e36bef48e6d4565be0f290da
haobingfu <haobingfu@frogsing.com>
2018-12-09 14:43:55 +0800

    modify commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
+/**
+ * 应用程序的根模块
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // NgModel lives here
+import { HttpClientModule  } from '@angular/common/http';//远程服务器通信的机制
+
+//使用内存wep api 模拟数据库
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +28,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    //forRoot配置方法接受一个InMemoryDataService类，（初期的内存数据库）作为参数
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService,{dataEncapsulation:false})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Hero } from './hero/hero';
 import { HEROES } from './hero/mock-heroes';
 import { Observable, of } from 'rxjs';
@@ -8,20 +10,33 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
-  constructor(private messageService: MessageService) { }
+  
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
   /**
    * of(HEROES)会返回一个Observable<Hero[]>，它会发出一个值，这个值就是这些英雄的数组。
    * @return {Observable<Hero[]>} [description]
    */
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  // getHeroes(): Observable<Hero[]> {
+  //   // TODO: send the message _after_ fetching the heroes
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return of(HEROES);
+  // }
+
+  getHeroes(): Observable<Hero[]>{
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHeroe(id: number): Observable<Hero>{
   	this.messageService.add(`HeroService: fetched heroes id=${id}`);
   	return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string){
+    this.messageService.add(`HeroService: ${message}`);
   }
   
 }
